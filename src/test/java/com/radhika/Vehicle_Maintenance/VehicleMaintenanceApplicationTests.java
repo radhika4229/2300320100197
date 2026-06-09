1c29001a8d8e4ab72361cd9cde7350580cba2f75
Radhika Sishodiya <sishodiyaradhika@gmail.com>
2026-06-09 11:28:50 +0530

    create logging middleware folder

---
+++ src/test/java/com/radhika/Vehicle_Maintenance/VehicleMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radhika.Vehicle_Maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
